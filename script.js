139eb06af4072901456e06ae97e5a04f4e0bc882
NITIN BARAI <46367193+nitin-Barai@users.noreply.github.com>
2021-08-21 14:16:59 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+(function($){
+  var typed =  new Typed ('span.txt-rotate',{
+      strings: ["I'm a GIS Developer and  GIS Data Analyst", " I'm a Cyber Forensic Specialist"],
+      typeSpeed: 80,
+      backSpeed: 100,
+      fadeout: false,
+      smartBackspace: true,
+      loop: true,
+  })
+})(jQuery);
+
+$(document).ready(function(){
+  $('.projects-wrapper').isotope({
+    filter:'*',
+    layoutMode: 'masonry'
+  });
+  $('.list-filter a').click(function(){
+    var selector = $(this).attr('data-filter');
+    $('.list-filter a').removeclass('active');
+    $(this).addclass('active');
+    $('.projects-wrapper').isotope({
+      filter: selector
+    });
+    return false;
+
+  });
+  $('.popup-image').magnificPopup({
+    type:'image',
+    closeOnContentClick: true,
+    gallary: {
+      enabled:true,
+      navigateByImgClick:true
+    }
+    
+  });
+  
+});
